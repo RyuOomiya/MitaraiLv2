13f9a6d20dc23a5879b5f3d009835df964677b58
ryuoomiya <oomiyaryu@gmail.com>
2023-05-09 15:35:33 +0900

    [Add] cubeにplayerが当たったらcubeが壊れるようにした

---
+++ Assets/Script/Cube.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+
+}
--- Assets/Script/InstantiateCube.cs
+++ Assets/Script/InstantiateCube.cs
@@ -8,14 +8,14 @@ public class InstantiateCube : MonoBehaviour
     [SerializeField] GameObject _cube;
     void Start()
     {
-
-    }
-
-    void Update()
-    {
         for (int i = -50; i <= 50; i += 5)
         {
             Instantiate(_cube, new Vector3(0, 1, i), Quaternion.identity);
         }
+    }
+
+    void Update()
+    {
+        
     }
 }
